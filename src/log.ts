53a42c4ba87a9aa523eee2c268fdd87decba7342
idleberg <git@idleberg.com>
2021-04-14 21:36:19 +0200

    replace logging module

--- src/log.ts
+++ src/log.ts
@@ -1,43 +1,7 @@
 import meta from '../package.json';
+import DeveloperConsole from '@atxm/developer-console';
 
-const consoleStyle = `
-  background-color: mediumpurple;
-  border-radius: 2px;
-  color: white;
-  line-height: 1.5;
-  padding: 1px 4px;
-  text-shadow: 0 1px 0px rgba(0, 0, 0, 0.2);
-`;
-
-function __console(type: string, ...args: unknown[]): void {
-  if (atom?.inDevMode()) {
-    args.unshift(`%c${meta.name}%c`, consoleStyle, '')
-    global.console[type](...args);
-  }
-}
-
-export default {
-  clear(...args: unknown[]): void {
-    __console('clear', ...args);
-  },
-
-  debug(...args: unknown[]): void {
-    __console('debug', ...args);
-  },
-
-  error(...args: unknown[]): void {
-    __console('error', ...args);
-  },
-
-  info(...args: unknown[]): void {
-    __console('info', ...args);
-  },
-
-  log(...args: unknown[]): void {
-    __console('log', ...args);
-  },
-
-  warn(...args: unknown[]): void {
-    __console('warn', ...args);
-  }
-}
+export default new DeveloperConsole({
+  name: meta.name,
+  backgroundColor: 'mediumpurple'
+});
